a7b9c2e5176488ccc0e017f488ad62058aef10da
Babur Usenakunov <bob.usenakunov@gmail.com>
2013-01-25 10:27:38 +0600

    Slides ordering added

--- app/controllers/spree/admin/slides_controller.rb
+++ app/controllers/spree/admin/slides_controller.rb
@@ -2,7 +2,16 @@ class Spree::Admin::SlidesController < Spree::Admin::ResourceController
   respond_to :html
 
   def index
-    @slides = Spree::Slide.all
+    @slides = Spree::Slide.ordered
   end
 
+  def update_positions
+    params[:positions].each do |id, index|
+      Spree::Slide.update_all(['position=?', index], ['id=?', id])
+    end
+
+    respond_to do |format|
+      format.js  { render :text => 'Ok' }
+    end
+  end
 end
--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,4 +1,6 @@
 class Spree::Slide < ActiveRecord::Base
   has_attached_file :image
+  acts_as_list
   scope :published, where(:published => true)
+  scope :ordered, order("#{Spree::Slide.table_name}.position ASC")
 end
